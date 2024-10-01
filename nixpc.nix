3bb463bb6c30eaa68fa26e72b754113c8855642a
ch4og <mitanick@ya.ru>
2024-10-01 16:26:20 +0300

    some changes and fixes

--- nixpc.nix
+++ nixpc.nix
@@ -39,6 +39,12 @@
         addresses = true;
       };
     };
+    btrfs.autoScrub = {
+      enable = true;
+      interval = "monthly";
+      fileSystems = [ "/" ];
+    };
+    fstrim.enable = true;
   };
 
   networking = {
@@ -52,11 +58,6 @@
     "/nix".options = [ "compress=zstd" "noatime" ];
     "/var/log".options = [ "compress=zstd" ];
     "/home/ch/Games".options = [ "compress=zstd" ];
-  };
-  services.btrfs.autoScrub = {
-    enable = true;
-    interval = "monthly";
-    fileSystems = [ "/" ];
   };
   zramSwap.enable = true;
   hardware.bluetooth.enable = true;
